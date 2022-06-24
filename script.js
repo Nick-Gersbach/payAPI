657dc68e0daa0496f30585eb106615ae9e9edcb2
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-06-24 12:36:22 -0700

    remove class fix

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ document
 document
   .querySelector(".close-menu")
   .addEventListener("click", () =>
-    document.querySelector(".mobile-nav").classList.removexxx("show")
+    document.querySelector(".mobile-nav").classList.remove("show")
   );
 
 function sendEmail(e) {
